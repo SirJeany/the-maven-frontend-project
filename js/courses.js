478f33162feaae5f89de0b108f69416ac58e18e3
SirJeany <jmvanriet1@gmail.com>
2020-03-13 10:55:08 +0200

    Added Front end dev to courses.js

--- js/courses.js
+++ js/courses.js
@@ -13,13 +13,21 @@ function Course(title, duration, distance, image, price) {
 
 // Courses prototypes:
 
+// Function to add course to cart:
 Course.prototype.addToCart = function() {
     cart.push(this);
     console.log(cart);
 }
 
+// Function to bin course from cart: 
+Course.prototype.removeFromCart = function() {
+    // Code to find the appropriate element in the array and remove it
+    // cart.pop();
+}
+
 // Some courses:
 let fullStackWebDev = new Course('Become a Full Stack Web Developer', '12 Week Course', 'In class', '/assets/images/full-stack-banner.png', 26000);
+let frontEndWebDev = new Course('Become a Front End Web Developer', '4 Week Course', 'In Class', '/assets/images/', 18000);
 
 // Cart:
 // Contains all courses that the user wishes to buy.
