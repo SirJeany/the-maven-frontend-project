3d14e55120f53f9121774526a9f9f75dd4df76b2
SirJeany <jmvanriet1@gmail.com>
2020-03-26 14:22:23 +0200

    CART JS: Adding enquiry now updates respective course to cart.

--- js/courses.js
+++ js/courses.js
@@ -15,7 +15,9 @@ function Course(title, duration, distance, image, price) {
 
 // Function to add course to cart:
 Course.prototype.addToCart = function() {
+    // ToDo: Add check to see if the course is already in the cart (surely we only want one course per type for now?) - Implement Promise?
     cart.push(this);
+    addCourseToCart(this);
     console.log(cart);
 }
 
@@ -26,9 +28,54 @@ Course.prototype.removeFromCart = function() {
 }
 
 // Some courses:
-let fullStackWebDev = new Course('Become a Full Stack Web Developer', '12 Week Course', 'In class', '/assets/images/full-stack-banner.png', 26000);
-let frontEndWebDev = new Course('Become a Front End Web Developer', '4 Week Course', 'In Class', '/assets/images/', 18000);
+let fullStackWebDev = new Course('Full Stack Web Developer', '12 Week Course', 'In Class', '/assets/images/full-stack-banner.png', 26000);
+let frontEndWebDev = new Course('Front End Web Developer', '4 Week Course', 'In Class', '/assets/images/frontend-dev.png', 18000);
 
 // Cart:
 // Contains all courses that the user wishes to buy.
 let cart = [];
+
+// CODE TO POPULATE COURSES ON PAGE:
+
+// CODE TO ADD COURSES TO CART:
+// Note:
+// 1) Buttons have an inline 'onclick' call that allows their respective events to fire.
+
+function addCourseToCart(course) {
+    let dropdownContainer = document.createElement('div');
+    dropdownContainer.classList.add('dropdown-item');
+    coursesContainer.appendChild(dropdownContainer); // Put the container in the cart.
+    
+    let flexContainer = document.createElement('div'); // Flex container wrapper
+    flexContainer.classList.add('d-flex');
+    dropdownContainer.appendChild(flexContainer);
+
+    let removeContainer = document.createElement('div'); // x button container
+    removeContainer.classList.add('d-flex', 'align-items-center', 'mr-4');
+    flexContainer.appendChild(removeContainer);
+
+    let removeBtnLink = document.createElement('a'); // x button a wrapper
+    removeBtnLink.classList.add('remove-from-cart-btn');
+    removeContainer.appendChild(removeBtnLink);
+
+    let removeBtnIcon = document.createElement('i'); // x button icon
+    removeBtnIcon.classList.add('fas', 'fa-times');
+    removeBtnLink.appendChild(removeBtnIcon);
+
+    let imgContainer = document.createElement('div'); // Course img container
+    imgContainer.classList.add('mr-3');
+    flexContainer.appendChild(imgContainer);
+
+    let courseImg = document.createElement('img'); // Course img fetched from course class
+    courseImg.setAttribute('src', course.image); // Set the attribute 'src' to be the appropriate course image.
+    courseImg.style = 'width: 94px'; // Update default image size
+    imgContainer.appendChild(courseImg);
+
+    let textContainer = document.createElement('div'); // Container holding all text deatailing course..
+    flexContainer.appendChild(textContainer);
+
+    let courseNameP = document.createElement('p'); // Heading part 'Become a Fullstack/webdev' ...
+    courseNameP.classList.add('text-primary', 'course-name-p');
+    courseNameP.textContent = 'Become a ' + course.title;
+    textContainer.appendChild(courseNameP);
+}
