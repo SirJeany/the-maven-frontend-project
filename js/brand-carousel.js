297081374ce756210ef9f017ac1bcacfa7665c31
SirJeany <jmvanriet1@gmail.com>
2020-03-28 09:06:22 +0200

    JS for Partners carousel

---
+++ js/brand-carousel.js
@@ -0,0 +1,30 @@
+// Credit to: Md. Ashiqur Rahman
+// https://codepen.io/mdashikar/pen/VWPvgE
+
+'use strict';
+
+$(document).ready(function() {
+    $(".customer-logos").slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 520,
+            settings: {
+            slidesToShow: 2
+            }
+        }
+        ]
+    });
+});
