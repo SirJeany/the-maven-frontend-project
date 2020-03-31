d456ba02d6d699e760462991cc88398acdac827f
SirJeany <jmvanriet1@gmail.com>
2020-03-31 15:08:59 +0200

    added dummy items to slider :)

---
+++ js/courses-carousel.js
@@ -0,0 +1,30 @@
+// Credit to: Md. Ashiqur Rahman
+// https://codepen.io/mdashikar/pen/VWPvgE
+
+'use strict';
+
+$(document).ready(function() {
+    $(".customer-logos").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 520,
+            settings: {
+            slidesToShow: 1
+            }
+        }
+        ]
+    });
+});
